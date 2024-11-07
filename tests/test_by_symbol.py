d5011af1bc37e0def168d7fbe19f76099f6f5091
Tammo Ippen <tammo.ippen@posteo.de>
2024-11-07 09:19:17 +0100

    fmt

--- tests/test_by_symbol.py
+++ tests/test_by_symbol.py
@@ -37,16 +37,16 @@ def test_by_symbol_match_filters_country_code(
 
 
 @pytest.mark.parametrize(
-    'text, expected_alpha3',
+    "text, expected_alpha3",
     (
         # symbol should be lowercase
-        ('lek', 'ALL'),
-        ('Lek', 'ALL'),
-        ('LEK', 'ALL'),
+        ("lek", "ALL"),
+        ("Lek", "ALL"),
+        ("LEK", "ALL"),
         # symbol should be uppercase
-        ('DH', 'AED'),
-        ('Dh', 'AED'),
-        ('dh', 'AED'),
+        ("DH", "AED"),
+        ("Dh", "AED"),
+        ("dh", "AED"),
     ),
 )
 def test_parse_by_symbol_value_is_case_insensitive(text, expected_alpha3):
@@ -56,28 +56,31 @@ def test_parse_by_symbol_value_is_case_insensitive(text, expected_alpha3):
 
 
 @pytest.mark.parametrize(
-    'text, ambiguous_alpha3, wanted_alpha3',
+    "text, ambiguous_alpha3, wanted_alpha3",
     (
         # ambiguous words + symbol
-        ('cost $100', 'WST', 'USD'),  # st$ => WST
+        ("cost $100", "WST", "USD"),  # st$ => WST
         # only letters found in words
-        ('durée 100', 'SZL', None),  # e => SWL / no currency
-        ('maximum 100', 'MRO', None),  # um => MRO / no currency
-        ('flowers', 'NPR', None),  # Re => NPR / no currency
-        ('flowers', 'LKR', None),  # Re => LKR / no currency
-        ('flowers', 'PKR', None),  # Re => PRK / no currency
-        ('amount : 100 currency : Ks', 'NPR', 'MMK'),  # Re => NPR / Ks is MMK
-        ('yes: 100l', 'SOS', 'ALL'),  # s gives SOS / l is ALL or LSL
+        ("durée 100", "SZL", None),  # e => SWL / no currency
+        ("maximum 100", "MRO", None),  # um => MRO / no currency
+        ("flowers", "NPR", None),  # Re => NPR / no currency
+        ("flowers", "LKR", None),  # Re => LKR / no currency
+        ("flowers", "PKR", None),  # Re => PRK / no currency
+        ("amount : 100 currency : Ks", "NPR", "MMK"),  # Re => NPR / Ks is MMK
+        ("yes: 100l", "SOS", "ALL"),  # s gives SOS / l is ALL or LSL
         # alpha 3 codes found in words
-        ('course', 'COU', None),  # COU => COU
-        ('finance', 'ANG', None),  # ANG => ANG
+        ("course", "COU", None),  # COU => COU
+        ("finance", "ANG", None),  # ANG => ANG
     ),
 )
 def test_parse_by_symbol_value_disambiguation(text, ambiguous_alpha3, wanted_alpha3):
-    assert iso4217parse.by_alpha3(ambiguous_alpha3) not in (iso4217parse.by_symbol_match(text) or [])
+    assert iso4217parse.by_alpha3(ambiguous_alpha3) not in (
+        iso4217parse.by_symbol_match(text) or []
+    )
 
     if wanted_alpha3:
-        assert iso4217parse.by_alpha3(wanted_alpha3) in iso4217parse.by_symbol_match(text)
+        assert iso4217parse.by_alpha3(wanted_alpha3) in iso4217parse.by_symbol_match(
+            text
+        )
     else:
         assert iso4217parse.by_symbol_match(text) is None
-
